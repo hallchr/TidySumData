128ae27f3ff2928776134e565dba05489118b279
Chris Hall <hallchr14@gmail.com>
2021-05-19 18:23:52 -0700

    initial commit

--- TidySumData.R
+++ TidySumData.R
@@ -1,4 +1,8 @@
 library(tidyverse)
-install.packages("here")
+#install.packages("here")
 library(here) #helps with setting working directories and finding paths
 library(ProjectTemplate) #helps codify a file layout hierarchy
+
+#create project template
+create.project(project.name = "ProjectTemplate",
+               template = "minimal")
