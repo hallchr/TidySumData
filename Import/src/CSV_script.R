435ef3fbde1993c9d44842bae444a83dd1a4cd58
Chris Hall <hallchr14@gmail.com>
2021-05-22 14:34:48 -0700

    CSV and TSV data

--- Import/src/CSV_script.R
+++ Import/src/CSV_script.R
@@ -14,3 +14,12 @@ head(df_csv)
 #skip = 2 will skip the first 2 rows. You can set the number to any number you want. This is helpful if there is additional information in the first few rows of your data frame that are not actually part of the table.
 #n_max = 100 will only read in the first 100 rows. You can set the number to any number you want. This is helpful if you’re not sure how big a file is and just want to see part of it.
 #By default, read_csv() converts blank cells to missing data (NA).
+
+#TSV files
+#Similar to CSVs but use tabs to seperate
+#Just a sample, wont run
+## read TSV into R
+df_tsv <- read_tsv("sample_data - Sheet1.tsv")
+
+## look at the object
+head(df_tsv)
