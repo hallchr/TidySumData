621d7ff9faeefb535556e3c966ab821a6e2417a2
Chris Hall <hallchr14@gmail.com>
2021-05-22 14:33:09 -0700

    initial commit

---
+++ Import/src/CSV_script.R
@@ -0,0 +1,16 @@
+#Reading CSVs
+
+## install.packages("readr")
+library(readr)
+library(here)
+
+## read CSV into R
+df_csv <- read_csv(here("data", "TMDL_data.csv"))
+
+## look at the object
+head(df_csv)
+
+#col_names = FALSE to specify that the first row does NOT contain column names.
+#skip = 2 will skip the first 2 rows. You can set the number to any number you want. This is helpful if there is additional information in the first few rows of your data frame that are not actually part of the table.
+#n_max = 100 will only read in the first 100 rows. You can set the number to any number you want. This is helpful if you’re not sure how big a file is and just want to see part of it.
+#By default, read_csv() converts blank cells to missing data (NA).
