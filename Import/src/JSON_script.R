bbf8d0024da5ed75f6d3bd3cbdb16e1ad2770ad1
Chris Hall <hallchr14@gmail.com>
2021-05-23 17:22:23 -0700

    initial commit of JSON

---
+++ Import/src/JSON_script.R
@@ -0,0 +1,29 @@
+#JSON files (javascript Object Notation)
+
+## generate a JSON object
+json <-
+  '[
+  {"Name" : "Woody", "Age" : 40, "Occupation" : "Sherriff"}, 
+  {"Name" : "Buzz Lightyear", "Age" : 34, "Occupation" : "Space Ranger"},
+  {"Name" : "Andy", "Occupation" : "Toy Owner"}
+]'
+
+## take a look
+json
+## [1] "[\n  {\"Name\" : \"Woody\", \"Age\" : 40, \"Occupation\" : \"Sherriff\"}, \n  {\"Name\" : \"Buzz Lightyear\", \"Age\" : 34, \"Occupation\" : \"Space Ranger\"},\n  {\"Name\" : \"Andy\", \"Occupation\" : \"Toy Owner\"}\n]"
+
+#install.packages("jsonlite")
+library(jsonlite)
+## Warning: package 'jsonlite' was built under R version 4.0.2
+
+
+## take JSON object and covert to a data frame
+JSONdf <- fromJSON(json)
+
+## take a look
+JSONdf
+
+## take JSON object and convert to a data frame
+json <- toJSON(JSONdf)
+json
+## [{"Name":"Woody","Age":40,"Occupation":"Sherriff"},{"Name":"Buzz Lightyear","Age":34,"Occupation":"Space Ranger"},{"Name":"Andy","Occupation":"Toy Owner"}]
