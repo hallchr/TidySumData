23afb5a1d262211c18d1bcc80bfbe110ac079b0b
hallchr <hallchr14@gmail.com>
2021-06-05 17:35:58 +0100

    case study 1 for healthcare.

--- Wrangling/src/Case study 1.R
+++ Wrangling/src/Case study 1.R
@@ -48,3 +48,64 @@ coverage <- coverage %>%
   left_join(state_data, by = "Location")
 
 coverage
+
+##Now to look at spending
+
+spending
+
+# take spending data from wide to long
+spending <- spending %>%
+  pivot_longer(-Location, 
+               names_to = "year", 
+               values_to = "tot_spending")
+
+# separate year and name columns
+spending <- spending %>% 
+  separate(year, sep="__", 
+           into = c("year", "name"), 
+           convert = TRUE) %>% 
+  select(-name) #select all columns except select....called negative indexing
+
+# look at the data
+spending
+
+hc <- inner_join(coverage, spending, 
+                 by = c("Location", "year"))
+
+hc
+
+hc <- hc %>% 
+  filter(Location != "United States") # remove the United states aggregate
+
+table(hc$type) #problem is the different types of coverage
+
+pop <- hc %>% 
+  filter(type == "Total") %>%              #The “Total” type is not really a formal type of healthcare coverage. It really represents just the total number of people in the state.
+  select(Location, year, tot_coverage)
+
+pop
+
+# ad population level information
+hc <- hc %>% 
+  filter(type != "Total") %>% 
+  left_join(pop, by = c("Location", "year")) %>% 
+  rename(tot_coverage = tot_coverage.x, 
+         tot_pop = tot_coverage.y)
+
+hc
+
+#calculate proportion of coverage
+#add proportion covered
+hc <- hc %>% 
+  mutate(prop_coverage = tot_coverage/tot_pop) 
+
+hc
+
+# get spending capita in dollars
+hc <- hc %>% 
+  mutate(spending_capita = (tot_spending*1e6) / tot_pop)
+
+hc
+#save to tidy data folder
+save(hc, file = here::here("data", "tidy_data", "case_study_1_tidy.rda"))
+
